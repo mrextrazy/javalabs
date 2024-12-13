a4354642954741cb8851652798ddb28b1eed5b6a
roman <romandorosenko891@gmail.com>
2024-12-13 20:57:03 +0200

    Розплутав іфи щоб було only one level of indentation per method

--- lab3/src/lab3/GildedRose.java
+++ lab3/src/lab3/GildedRose.java
@@ -12,51 +12,38 @@ class GildedRose {
         	boolean isAgedBrie = items[i].name.equals("Aged Brie");
             boolean isBackstagePass = items[i].name.equals("Backstage passes to a TAFKAL80ETC concert");
             boolean isSulfuras = items[i].name.equals("Sulfuras, Hand of Ragnaros");
-            if (!isAgedBrie && !isBackstagePass) {
-                if (items[i].quality > 0) {
-                    if (!isSulfuras) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (isBackstagePass) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+            if (!isAgedBrie && !isBackstagePass && !isSulfuras && items[i].quality > 0 ) {
+                items[i].quality--;
             }
 
+            if (isAgedBrie || isBackstagePass) {
+                if (items[i].quality < 50) {
+                    items[i].quality++;
+                }
+            }
+            if (isBackstagePass) {
+                if (items[i].sellIn < 11 && items[i].quality < 50) {
+                    items[i].quality++;
+                }
+                if (items[i].sellIn < 6 && items[i].quality < 50) {
+                    items[i].quality++;
+                }
+            }
             if (!isSulfuras) {
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn--;
             }
 
             if (items[i].sellIn < 0) {
-                if (!isAgedBrie) {
-                    if (!isBackstagePass) {
-                        if (items[i].quality > 0) {
-                            if (!isSulfuras) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                if (!isAgedBrie && !isBackstagePass && !isSulfuras && items[i].quality > 0 ) {
+                    items[i].quality--;
+                }
+
+                if (isBackstagePass) {
+                    items[i].quality = 0;
+                }
+
+                if (isAgedBrie && items[i].quality < 50) {
+                    items[i].quality++;
                 }
             }
         }
